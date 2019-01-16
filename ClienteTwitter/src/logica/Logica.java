c5a9158dd708a8b5fd5d579c2a95a3ec5702d2dd
Ainhoa Valle <a.ainhoavalle@gmail.com>
2019-01-16 14:13:10 +0100

    inicioAinhoa

---
+++ ClienteTwitter/src/logica/Logica.java
@@ -0,0 +1,18 @@
+package logica;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+
+/**
+ *
+ * @author Ainhoa
+ */
+public class Logica {
+    
+    Twitter twitter = TwitterFactory.getSingleton();
+
+
+    
+    
+    
+}

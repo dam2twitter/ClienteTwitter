cc5e867c587ff53aac512d1102b79ce8d5d1d3aa
RogeLS <rogelzsg@gmail.com>
2019-01-24 18:50:55 +0100

    Primer comentario

--- skinlf-1.2.3/examples/clock.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo If you are using JDK1.4 and it complains about jawt.dll, use jdk.home\jre\bin\java instead of jdk.home\bin\java
-set OLDPATH=%PATH%
-set PATH=%PATH%;..\lib
-java -classpath ..\lib\nativeskin.jar;demo.jar Clock clock.gif
-set PATH=%OLDPATH%
-
-
--- skinlf-1.2.3/examples/demo.bat
+++
@@ -1,4 +0,0 @@
-set THEMEPACK=%1
-IF "%THEMEPACK%"=="" SET THEMEPACK=..\lib\themepack.zip
-java -classpath ..\lib\skinlf.jar;demo.jar demo %THEMEPACK% %2 %3 %4 %5 %6 %7 %8 %9
-
